47cf2f5961458c533a2c8573ae8847d54210e35f
Unmeinh <117454523+Unmeinh@users.noreply.github.com>
2023-05-26 22:45:40 +0700

    Delete BackendAgile directory

--- BackendAgile/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var BaiVietController = require("../Controller/BaiViet");
-var router = express.Router();
-
-router.get("/listBaiViet/:idNguoiDung", BaiVietController.listBaiViet);
-
-module.exports = router;
